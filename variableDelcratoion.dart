ff991f18499cf52d105f2fd9833ba3cc3627b9ab
Sadik Saroar <91769816+sadiksaroar@users.noreply.github.com>
2025-03-21 21:55:38 +0600

    Add files via upload

---
+++ variableDelcratoion.dart
@@ -0,0 +1,21 @@
+// Example Combining Keywords, Variables, and Data Types
+
+void main() {
+  // varible declation
+
+  var name = "Alice";
+
+  int age = 25; //
+  final String country = "USA";
+  const double pi = 3.1416;
+
+  dynamic value = 10;
+  value = "Hello";
+
+  List<int> numbers = [1, 2, 3];
+  Map<String, int> ages = {"Allice ": 25, "Bob": 30};
+
+  // Printing values
+  print("Name: $name, Age: $age, Country: $country");
+  print("Numbers: $numbers, Ages: $ages");
+}
